f23fcf5603fce0b77ff9caf6ab6690300d6d5ce2
Chris Aprea <chris.aprea@gmail.com>
2013-08-16 16:19:48 +1000

    Convert spacing to tabs instead of spaces

--- wp-migrate-db-pro-tweaks.php
+++ wp-migrate-db-pro-tweaks.php
@@ -21,53 +21,53 @@ Author URI: http://deliciousbrains.com
 
 class WP_Migrate_DB_Pro_Tweaks {
 
-    function __construct() {
-        if ( !is_admin() ) return;
-        add_action( 'init', array( $this, 'init' ), 9 );
-    }
+	function __construct() {
+		if ( !is_admin() ) return;
+		add_action( 'init', array( $this, 'init' ), 9 );
+	}
 
-    function init() {
-        // Uncomment the following lines to initiate a filter
+	function init() {
+		// Uncomment the following lines to initiate a filter
 
-        //add_filter( 'wpmdb_migration_complete', array( $this, 'migration_complete', 10, 2 );
-        //add_filter( 'wpmdb_bottleneck', array( $this, 'bottleneck' ), 10, 2 );
-        //add_filter( 'wpmdb_sensible_pull_limit', array( $this, 'sensible_pull_limit' ), 10, 2 );
-        //add_filter( 'wpmdb_temporary_prefix', array( $this, 'temporary_prefix' ) );
-    }
+		//add_filter( 'wpmdb_migration_complete', array( $this, 'migration_complete', 10, 2 );
+		//add_filter( 'wpmdb_bottleneck', array( $this, 'bottleneck' ), 10, 2 );
+		//add_filter( 'wpmdb_sensible_pull_limit', array( $this, 'sensible_pull_limit' ), 10, 2 );
+		//add_filter( 'wpmdb_temporary_prefix', array( $this, 'temporary_prefix' ) );
+	}
 
-    // Override the temporary table name prefix
-    function temporary_prefix( $prefix ) {
-        return 'mig_';
-    }
+	// Override the temporary table name prefix
+	function temporary_prefix( $prefix ) {
+		return 'mig_';
+	}
 
-    // Force bottleneck
-    function bottleneck( $bytes ) {
-        return 1024 * 1024; // 1MB
-    }
+	// Force bottleneck
+	function bottleneck( $bytes ) {
+		return 1024 * 1024; // 1MB
+	}
 
-    // Force pull limit
-    function sensible_pull_limit( $bytes ) {
-        return 1024 * 1024; // 1MB
-    }
+	// Force pull limit
+	function sensible_pull_limit( $bytes ) {
+		return 1024 * 1024; // 1MB
+	}
 
-    // Counts migrations by connection URL and migration type
-    function migration_complete( $migration_type, $connection_url ) {
-        $slug = 'wpmdbpro_stats';
-        
-        $stats = get_option( $slug );
-        if ( !$stats ) {
-            $stats = array();
-        }
+	// Counts migrations by connection URL and migration type
+	function migration_complete( $migration_type, $connection_url ) {
+		$slug = 'wpmdbpro_stats';
+		
+		$stats = get_option( $slug );
+		if ( !$stats ) {
+			$stats = array();
+		}
 
-        if ( isset( $stats[$connection_url][$migration_type] ) ) {
-            $stats[$connection_url][$migration_type] = $stats[$connection_url][$migration_type] + 1;
-        }
-        else {
-            $stats[$connection_url][$migration_type] = 1;
-        }
-        
-        update_option( $slug, $stats );
-    }
+		if ( isset( $stats[$connection_url][$migration_type] ) ) {
+			$stats[$connection_url][$migration_type] = $stats[$connection_url][$migration_type] + 1;
+		}
+		else {
+			$stats[$connection_url][$migration_type] = 1;
+		}
+		
+		update_option( $slug, $stats );
+	}
 }
 
 new WP_Migrate_DB_Pro_Tweaks();
