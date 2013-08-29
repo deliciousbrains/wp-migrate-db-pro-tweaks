65d92eb1fc9309bfb5b6e299c71718cb52a2e8a2
Chris Aprea <chris.aprea@gmail.com>
2013-08-29 16:41:48 +1000

    Bottleneck only has 1 argument

--- wp-migrate-db-pro-tweaks.php
+++ wp-migrate-db-pro-tweaks.php
@@ -30,7 +30,7 @@ class WP_Migrate_DB_Pro_Tweaks {
 		// Uncomment the following lines to initiate an action / filter
 
 		//add_filter( 'wpmdb_migration_complete', array( $this, 'migration_complete' ), 10, 2 );
-		//add_filter( 'wpmdb_bottleneck', array( $this, 'bottleneck' ), 10, 2 );
+		//add_filter( 'wpmdb_bottleneck', array( $this, 'bottleneck' ) );
 		//add_filter( 'wpmdb_sensible_pull_limit', array( $this, 'sensible_pull_limit' ), 10, 2 );
 		//add_filter( 'wpmdb_temporary_prefix', array( $this, 'temporary_prefix' ) );
 		//add_filter( 'wpmdb_upload_info', array( $this, 'upload_info' ) );
